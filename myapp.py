44ad109d6e8bd78692acdaeabdb3ce14173ccd5c
Michael Essien <essienmichael4@gmail.com>
2019-07-24 14:59:24 +0000

    not done

--- myapp.py
+++ myapp.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 
 
-@app.route('/', methods=['GET', 'POST'])
+#@app.route('/', methods=['GET', 'POST'])
 #def index():
 #    temp = ""
  #   if request.method == 'POST':
@@ -13,27 +13,29 @@ app = Flask(__name__)
    #         temp = "10"
 
     #return render_template('index.html', temperature = temp )
-def get_data():
-    return requests.get('https://192.168.43.231:5000/api/humidity').content
+#def get_data():
+#    return requests.get('http://192.168.43.231:5000/api/humidity').content
     
 
 
 
 
-#@app.route('/', methods=['GET', 'POST'])
-#def index():
-#    temp = ""
-#    if request.method == 'POST':
-#        if request.form.get('temperature')=='temperature':
-#            temp = "10"
-
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    temp = ""
+    if request.method == 'POST':
+        if request.form.get('temperature')=='temperature':
+            def get_data():
+                return requests.get('http://192.168.43.231:5000/api/humidity').content
+            temp = requests.get('http://192.168.43.231:5000/api/humidity').content
 #   return 'Hello world'
-#    return render_template('index.html', temperature = temp )
+#print(temp.decode('utf-8'))
+    return render_template('index.html', temperature = temp )
 
-@app.route('/temperature/', methods=['POST'])
-def temperature():
-    print("10")
-    return render_template('index.html')
+#@app.route('/temperature/', methods=['POST'])
+#def temperature():
+#    print("10")
+#    return render_template('index.html')
 
 
 if __name__ == '__main__':
