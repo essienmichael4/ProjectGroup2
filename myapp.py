d52b5994f81959a3ba8264c77d96cb3938c80d51
Michael Essien <essienmichael4@gmail.com>
2019-07-24 13:43:41 +0000

    all done

--- myapp.py
+++ myapp.py
@@ -1,34 +1,39 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import requests
+import pyowm
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-#	return 'Hello world'
-	return render_template('index.html')
 
-#@app.route('/about')
-#def about():
-#        return render_template('about.html')
 
-#@app.route('/signup')
-#def signup():
-#        return render_template('signup.html')
+@app.route('/', methods=['GET', 'POST'])
+#def index():
+#    temp = ""
+ #   if request.method == 'POST':
+  #      if request.form.get('temperature')=='temperature':
+   #         temp = "10"
 
-#@app.route('/services')
-#def services():
-#        return render_template('services.html')
+    #return render_template('index.html', temperature = temp )
+def get_data():
+    return requests.get('https://192.168.43.231:5000/api/humidity').content
+    
 
-#@app.route('/login')
-#def login():
-#        return render_template('login.html')
 
-#@app.route('/contacts')
-#def contacts():
-#        return render_template('contacts.html')
 
-#@app.route('/whereami')
-#def whereami():
-#	return 'Ghana!'
+
+#@app.route('/', methods=['GET', 'POST'])
+#def index():
+#    temp = ""
+#    if request.method == 'POST':
+#        if request.form.get('temperature')=='temperature':
+#            temp = "10"
+
+#   return 'Hello world'
+#    return render_template('index.html', temperature = temp )
+
+@app.route('/temperature/', methods=['POST'])
+def temperature():
+    print("10")
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
