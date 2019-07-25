f0b11105cd4e3963f148bca225ed118ce5ac4c8d
Michael Essien <essienmichael4@gmail.com>
2019-07-25 12:11:28 +0000

    Almost done

--- myapp.py
+++ myapp.py
@@ -1,36 +1,46 @@
 from flask import Flask, render_template, request
 import requests
+import json
 import pyowm
 app = Flask(__name__)
 
 
+def get_data():
+    return request.get('http://192.168.43.231:5000/api/humidity').content
+    
+    
+@app.route('/', methods=['GET', 'POST'])
+def index():
+	temp = ""
+	humid = ""
+	if request.method == 'POST':
+		if request.form.get('temperature')=='temperature':
 
-#@app.route('/', methods=['GET', 'POST'])
-#def index():
-#    temp = ""
- #   if request.method == 'POST':
-  #      if request.form.get('temperature')=='temperature':
-   #         temp = "10"
+			temp = requests.get('http://192.168.43.231:5000/api/temperature').json()['temperature']
 
-    #return render_template('index.html', temperature = temp )
+
+		if request.form.get('humidity')=='humidity':
+
+			humid = requests.get('http://192.168.43.231:5000/api/humidity').json()['humidity']
+	return render_template('index.html', temperature = temp, humidity = humid)
 #def get_data():
-#    return requests.get('http://192.168.43.231:5000/api/humidity').content
+    #return requests.get('http://192.168.43.231:5000/api/humidity').content
     
 
 
 
 
-@app.route('/', methods=['GET', 'POST'])
-def index():
-    temp = ""
-    if request.method == 'POST':
-        if request.form.get('temperature')=='temperature':
-            def get_data():
-                return requests.get('http://192.168.43.231:5000/api/humidity').content
-            temp = requests.get('http://192.168.43.231:5000/api/humidity').content
-#   return 'Hello world'
-#print(temp.decode('utf-8'))
-    return render_template('index.html', temperature = temp )
+#@app.route('/', methods=['GET', 'POST'])
+#def index():
+#    humidity_response = ""
+#    if request.method == 'POST':
+#        if request.form.get('temperature')=='temperature':
+#            def get_data():
+#                humidity_response = request.get('http://192.168.43.231:5000/api/humidity').content
+#
+##    humidity_json = json.loads(humidity_response)
+#    #print(humidity_json)
+#            return render_template('index.html')
 
 #@app.route('/temperature/', methods=['POST'])
 #def temperature():
@@ -39,4 +49,4 @@ def index():
 
 
 if __name__ == '__main__':
-        app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0')
